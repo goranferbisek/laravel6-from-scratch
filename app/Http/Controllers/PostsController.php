9f45a480476b957f78d0f269a219fb5d5d4c5fcd
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-01-19 21:06:59 +0100

    Read a post from a database

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,19 +6,12 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first post!',
-            'my-second-post' => 'My second post!'
-        ];
-
-        if (! array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found');
-        }
+        $post = \DB::table('posts')->where('slug', $slug)->first();
 
         return view('post',[
-            'post' => $posts[$post]
+            'post' => $post
         ]);
     }
 }
