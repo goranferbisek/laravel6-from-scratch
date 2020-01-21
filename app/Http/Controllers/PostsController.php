8ee9158d6f1d33fa6d0236ee0a7cecc6855ddfee
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-01-21 18:35:41 +0100

    Replace Query Builder with an Eloquent model

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-
         return view('post',[
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
