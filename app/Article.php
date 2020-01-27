6b07e1291457a899601ffd350e4c331ee8e0043b
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-01-27 17:10:49 +0100

    Fix: mass assigment error

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    // Protection from mass assignment
+    // Example of a dangerous assignment: User::create(request()->all())
+    //protected $fillable = ['title', 'excerpt', 'body'];
+
+    // In case we do not need protection
+    protected $guarded = [];
 }
